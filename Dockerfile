8c992a28a24b3a3cd25d6c90961966b9de9c16fb
Kevin Gallagher <k.gallagher@fct.unl.pt>
2023-04-13 15:58:06 +0100

    Add Dockerfile and related script changes.

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ ENV PATH="/scripts:${PATH}"
 
 RUN mkdir /code
 RUN mkdir /scripts
+RUN mkdir /data
 
 ADD . /code/
 
 RUN mv /code/scripts/* /scripts/
+RUN mv /code/data/* /data/ 
 RUN chmod +x /scripts/*
 WORKDIR /code
 
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-gradle -g gradle_user_home -v
-
 if [[ -z "${TREE_ROLE}" ]]; then
     ROLE="ALL"
 else
@@ -13,13 +11,13 @@ if [[ $ROLE == "ALL" ]]; then
     gradle -g gradle_user_home test;
 elif [[ $ROLE == "SERVER" ]]; then
     gradle -g gradle_user_home assemble;
-    gradle -g gradle_user_home server;
-elif [[ $ROLE == "LEVEL_SITE" && -n ${LEVEL_SITE} ]]; then
+    gradle -g gradle_user_home run -PchooseRole=weka.finito.server_site;
+elif [[ $ROLE == "LEVEL_SITE" ]]; then #&& -n ${LEVEL_SITE} ]]; then
     gradle -g gradle_user_home assemble;
-    gradle -g gradle_user_home level ${LEVEL_SITE};
+    gradle -g gradle_user_home run -PchooseRole=weka.finito.level_site_server;
 elif [[ $ROLE == "CLIENT" ]]; then
     grade -g gradle_user_home assemble;
-    gradle -g gradle_user_home client;
+    gradle -g gradle_user_home run -PchooseRole=weka.finito.client;
 else
     echo "Sorry, this is not a valid MPC-PPDT role. Please try again."
     exit
