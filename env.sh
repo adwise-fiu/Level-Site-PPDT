47eafeeee221d0294976919dec9ae601f20af5ac
AndrewQuijano <andrewquijano92@gmail.com>
2023-04-26 23:28:16 -0400

    The Kuberenetes jobs run, but I can't see updates to level-site or anything printed, I will ask Kevin. Also, I confirmed the gradle commands inthe job files run on the CLI just fine

--- env.sh
+++ env.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export PRECISION=2
-export PPDT_DATA_DIR="./data/"
 export PPDT_KEY_SIZE=1024
 export PORT_NUM=9000
-export LEVEL_SITE_DOMAINS="levelsite01,levelsite02,levelsite03,levelsite04,levelsite05,levelsite06,levelsite07,levelsite08,levelsite09,levelsite10"
-export VALUES="hypothyroid.values"
-export TRAINING="hypothyroid.arff"
+export LEVEL_SITE_DOMAINS="ppdt-level-site-01-service,ppdt-level-site-02-service,ppdt-level-site-03-service,ppdt-level-site-04-service,ppdt-level-site-05-service,ppdt-level-site-06-service,ppdt-level-site-07-service,ppdt-level-site-08-service,ppdt-level-site-09-service,ppdt-level-site-10-service"
 export TREE_ROLE="SERVER"
-export TIME_METHODS="false"
